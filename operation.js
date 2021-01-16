5e314ca40916cb65815aa4cc8f1dfec101c19820
unknown <niambaust17@gmail.com>
2021-01-16 14:56:24 +0600

    initial commit

---
+++ operation.js
@@ -0,0 +1,29 @@
+var price1 = 35;
+var price2 = 25;
+console.log(price1 + price2);
+console.log(price1 - price2);
+console.log(price1 * price2);
+console.log(price1 / price2);
+console.log(price1 % price2);
+price1++;
+console.log(price1);
+price1--;
+console.log(price1);
+var firstName = "Niam";
+var lastName = "Md. Affan"
+console.log(firstName + " " + lastName)
+console.log(price1 + firstName);
+
+// Math function operation
+var negativeNumber = -17;
+console.log(Math.abs(negativeNumber));
+var fracNumber1 = 20.23;
+var fracNumber2 = 20.53;
+console.log(Math.round(fracNumber1));
+console.log(Math.round(fracNumber2));
+
+console.log(Math.ceil(fracNumber1));
+console.log(Math.floor(fracNumber1));
+
+var randomNumber = Math.random() * 100;
+console.log(Math.floor(randomNumber));
---
+++ variable.js
@@ -0,0 +1,42 @@
+var studentId = 170101017;
+var completeBsc = 2021;
+var studentName = "Niam Md. Affan";
+var studentDepartment = "CSE";
+var isMarried = false;
+var isAlcoholic = false;
+console.log(studentId);
+console.log(studentName);
+console.log(isMarried);
+console.log(completeBsc);
+console.log(studentDepartment);
+console.log(isAlcoholic);
+
+// variable type find out
+console.log(typeof completeBsc);
+console.log(typeof studentDepartment);
+console.log(typeof isAlcoholic);
+
+// string case change and findout index of specific word or split longer string
+console.log(studentName.toLowerCase());
+console.log(studentName.toUpperCase());
+console.log(studentName.indexOf('an'));
+console.log(studentName.indexOf('An'));
+console.log(studentName.split(' '));
+
+// type conversion
+var numberOne = 15;
+var numberTwo = 25.45;
+var numberThree = "17";
+var numberFour = "17.71";
+console.log(parseInt(numberOne + numberTwo));
+console.log(parseFloat(numberOne + numberTwo));
+console.log(parseInt(numberThree));
+console.log(typeof parseInt(numberThree));
+// string to int/float +(string) or parseInt/parseFloat
+console.log(typeof +(numberFour));
+console.log(+(numberThree) + +(numberFour));
+
+var numberFive = 0.1;
+var numberSix = 0.2;
+var total = numberFive + numberSix;
+console.log(total.toFixed(3));
